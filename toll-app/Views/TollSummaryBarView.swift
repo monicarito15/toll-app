251eb3ea4c3ef789c62c95e31fd55edec96465f1
Carolina <carolinamera1985@gmail.com>
2026-01-14 20:12:37 +0100

    make summaryView for total of tolls passing the route, and detailsView

---
+++ toll-app/Views/TollSummaryBarView.swift
@@ -0,0 +1,22 @@
+//
+//  TollResultView.swift
+//  toll-app
+//
+//  Created by Carolina Mera  on 14/01/2026.
+//
+import SwiftUI
+
+struct TollSummaryBarView: View {
+    
+    let tollCount: Int
+    let base: Double
+    let timeAdj: Double
+    let total: Double
+    let date: Date
+    
+    var body: some View {
+        VStack {
+            Text("Toll Result View")
+        }
+    }
+}
